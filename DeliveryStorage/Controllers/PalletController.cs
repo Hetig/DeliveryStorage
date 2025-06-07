1b35ca08ed962c59ba08f6cc095c83c329834f89
Hetag <kh.ha4irov2011@yandex.ru>
2025-06-07 19:09:14 +0300

    Add Projects

---
+++ DeliveryStorage/Controllers/PalletController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryStorage.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PalletController : ControllerBase
+    {
+
+    }
+}
--- DeliveryStorage/Program.cs
+++ DeliveryStorage/Program.cs
@@ -1,19 +1,51 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using DeliveryStorage.Database.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+ 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DeliveryStorage API", Version = "v1" });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+});
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Host.UseSerilog();
+
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseRouting();
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+await context.Database.MigrateAsync();
 
 app.Run();
